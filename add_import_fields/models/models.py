ede7fc8f59b8f2480607868fb48d321bfdc4cb20
pabloosorio <35545995+pabloosorio@users.noreply.github.com>
2018-07-31 23:31:18 -0500

    Update models.py
    cambio en lineas de suma para el importe ejercido y modificación al query para el calculo del importe comprometido

--- add_import_fields/models/models.py
+++ add_import_fields/models/models.py
@@ -9,18 +9,20 @@ class AddFields(models.Model):
 	imcomprometido = fields.Float(string="Importe Comprometido",compute="_valorr")
 
 
-	@api.one
+
 	@api.depends('planned_amount','practical_amount')
 	def _valor(self):
-		self.imejercido = self.planned_amount + self.practical_amount
-		
-	@api.one
+		if(self.practical_amount<=0):
+			self.imejercido = self.planned_amount + self.practical_amount
+		else:
+			self.imejercido = self.planned_amount - self.practical_amount
+
 	@api.depends('general_budget_id','imcomprometido')
 	def _valorr(self):
 		comp=0
 		for l in self.general_budget_id.account_ids:
 			cr = self.env.cr
-			sql = "select COALESCE(sum(ail.price_total),0) from account_invoice_line ail inner join account_invoice ai on ai.id=ail.invoice_id where ai.state='draft' and ail.account_id='"+str(l.id)+"'"
+			sql = "select COALESCE(sum(ail.price_total),0) from account_invoice_line ail inner join account_invoice ai on ai.id=ail.invoice_id where ai.state='draft' and ai.type='out_invoice' and ail.account_id='"+str(l.id)+"'"
 			cr.execute(sql)
 			m = cr.fetchone()
 			if m is None:
