ad29c12c94736787ec157720bd00b71302ade460
pabloosorio <35545995+pabloosorio@users.noreply.github.com>
2018-08-01 08:06:27 -0500

    Update models.py

--- add_import_fields/models/models.py
+++ add_import_fields/models/models.py
@@ -12,7 +12,7 @@ class AddFields(models.Model):
 	@api.one
 	@api.depends('planned_amount','practical_amount')
 	def _valor(self):
-		self.imejercido = self.planned_amount - self.practical_amount
+		self.imejercido = self.planned_amount + self.practical_amount
 		
 	@api.one
 	@api.depends('general_budget_id','imcomprometido')
@@ -20,7 +20,7 @@ class AddFields(models.Model):
 		comp=0
 		for l in self.general_budget_id.account_ids:
 			cr = self.env.cr
-			sql = "select COALESCE(sum(ail.price_total),0) from account_invoice_line ail inner join account_invoice ai on ai.id=ail.invoice_id where ai.state='draft' and ai.type='out_invoice' and ail.account_id='"+str(l.id)+"'"
+			sql = "select COALESCE(sum(ail.price_total),0) from account_invoice_line ail inner join account_invoice ai on ai.id=ail.invoice_id where ai.state='draft' and ail.account_id='"+str(l.id)+"'"
 			cr.execute(sql)
 			m = cr.fetchone()
 			if m is None:
