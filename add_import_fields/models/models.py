d4dea980e4d5c51ac2dd276f7e8aa84b79f77b1c
pabloosorio <35545995+pabloosorio@users.noreply.github.com>
2018-08-01 01:31:25 -0500

    Update models.py

--- add_import_fields/models/models.py
+++ add_import_fields/models/models.py
@@ -5,16 +5,18 @@ from odoo import models, fields, api
 class AddFields(models.Model):
 	_inherit = "crossovered.budget.lines"
 
-	imejercido = fields.Float(string="Importe Ejercido",compute="amejer")
-	imcomprometido = fields.Float(string="Importe Comprometido",compute="sum_flines")
+	imejercido = fields.Float(string="Importe Ejercido",compute="_valor")
+	imcomprometido = fields.Float(string="Importe Comprometido",compute="_valorr")
 
+
+	@api.one
 	@api.depends('planned_amount','practical_amount')
-	def amejer(self):
+	def _valor(self):
 		self.imejercido = self.planned_amount - self.practical_amount
-
-
+		
+	@api.one
 	@api.depends('general_budget_id','imcomprometido')
-	def sum_flines(self):
+	def _valorr(self):
 		comp=0
 		for l in self.general_budget_id.account_ids:
 			cr = self.env.cr
@@ -22,6 +24,6 @@ class AddFields(models.Model):
 			cr.execute(sql)
 			m = cr.fetchone()
 			if m is None:
-				m=0
+				m=(0,)
 			comp = comp + max(m)
 		self.imcomprometido = comp
